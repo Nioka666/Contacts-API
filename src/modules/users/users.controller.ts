a144eb9b1b199c9a54687156e7f382c13e9a3a6b
Adhim Nioka <niokagi@gmail.com>
2024-03-17 12:12:02 +0700

    First Commit

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,17 +1,15 @@
 import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
-import { Response } from 'express';
 import { UsersList } from './data/usersList';
 
 @Controller('users')
-
 export class UsersController {
   @Get('/lists')
-  getUsersList(@Res() res: Response) {
+  getUsersList(@Res() res: any) {
     res.status(HttpStatus.OK).json(UsersList);
   }
 
   @Get('get-user/:userID')
-  getUserByID(@Param('userID') userID: string, @Res() res: Response) {
+  getUserByID(@Param('userID') userID: string, @Res() res: any) {
     res.json({ userID: `${userID}` });
   }
 }
